1c1b70d666920b78b206adc3968b68a5a92078bb
Dina Khodaee <153456739+DinaKhodaee@users.noreply.github.com>
2023-12-28 17:02:05 +0330

    Update tamrin 4 - 2.py

--- tamrin 4 - 2.py
+++ tamrin 4 - 2.py
@@ -4,5 +4,6 @@ print(f.renderText("Dina Khodaee"))
 
 h = int(input("Enter your time by hour: "))
 m = int(input("Enter your time by minute: "))
-sec = h*3600 + m*60
+s = int(input("Enter your time by second: "))
+sec = h*3600 + m*60 + s
 print("Your time by second: ", sec)
